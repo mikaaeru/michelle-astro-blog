2260764fab302f49ba5c1b1d484af179a1b52ae8
Michelle S. W. <sel@kamikami.eu>
2026-03-06 01:40:21 +0700

    shit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Michelle's SHIT BLOG";
-export const SITE_DESCRIPTION = "When I hate technology so much, I ramble about those shit here";
+export const SITE_TITLE = "Michelle's SHIT";
+export const SITE_DESCRIPTION = "When I hate technology so much, I shit words here";
