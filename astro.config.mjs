d62a23c92d0449e7b7966691753eec2b2b22117b
Michelle S. W. <sel@kamikami.eu>
2026-03-06 11:59:26 +0700

    SSR disable

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
 	site: "https://example.com",
-	output: "server", // <-- ADD THIS LINE
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
