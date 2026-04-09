1fed5ba3068d3863579875e44111f88498771ecb
Michelle S. W. <sel@kamikami.eu>
2026-04-09 21:15:15 +0700

    fix link

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Michelle's SHIT";
-export const SITE_DESCRIPTION = "When I hate technology so much, I shit words here";
+export const SITE_TITLE = "Michelle's DNS and SHIT BLOG";
+export const SITE_DESCRIPTION = "Michelle's DNS is a WSF-approved profile to provision Apple iOS/iPadOS devices to allow sideloading using revoked certificate. Updates to anything I do on/for the internet community is posted on the blog. STAY TUNED!";
